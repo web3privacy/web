40d365c0dc1bd300309ac801068e5ce222fe5254
tree <tree@tree.fail>
2024-01-25 18:49:03 +0100

    update

--- utils/explorer.js
+++ utils/explorer.js
@@ -3,8 +3,12 @@
 const resp = await fetch("https://explorer-data.web3privacy.info/");
 const data = await resp.json();
 
+const respContribs = await fetch(`https://api.github.com/repos/web3privacy/explorer-data/contributors`)
+const respContribsData = await respContribs.json();
+
 const explorer = {
-    projects: data.projects.length
+    projects: data.projects.length,
+    contributors: respContribsData.length,
 }
 
 await Deno.writeTextFile("./src/explorer.json", JSON.stringify(explorer, null, 2))
