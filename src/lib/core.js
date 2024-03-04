126c5401768517776f9597b82838bf150861e805
tree <tree@hidden.cz>
2024-03-04 02:38:21 +0100

    research page rework

---
+++ src/lib/core.js
@@ -0,0 +1,18 @@
+import core from "../core.json";
+
+export function getPersonByGH(user) {
+    return core.people.find(p => p.refs?.github?.toLowerCase() === user.toLowerCase())
+}
+
+export function personLink (p) {
+    if (p.refs?.twitter) {
+        return `https://twitter.com/${p.refs.twitter}`
+    }
+    if (p.refs?.bsky) {
+        return `https://bsky.app/profile/${p.refs.bsky}`
+    }
+    if (p.refs?.github) {
+        return `https://github.com/${p.refs.github}`
+    }
+    return '#'
+}
