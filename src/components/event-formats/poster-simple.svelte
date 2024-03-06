7d180bb4a2feb9dcd338c83bb2316d65de21e3e0
tree <tree@hidden.cz>
2024-03-06 23:27:07 +0100

    update

--- src/components/event-formats/poster-simple.svelte
+++ src/components/event-formats/poster-simple.svelte
@@ -27,7 +27,7 @@
     {:else}
         <div class="absolute top-28 left-0 h-32 w-full z-10" style="background: linear-gradient(black, transparent);"></div>
         <div class="absolute top-28 left-[-70px] w-[900px] h-[900px] -z-0" style="background-image: url({image}); background-size: 100% 100%; background-position: center;"></div>
-        <div class="absolute bottom-[12em] left-0 h-48 w-full z-10" style="background: linear-gradient(transparent, black);"></div>
+        <div class="absolute bottom-[7em] left-0 h-48 w-full z-10" style="background: linear-gradient(transparent, black);"></div>
 
         <div class="absolute bottom-10 left-10 uppercase text-3xl z-20">
             <div class="flex">
